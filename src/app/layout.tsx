d7780affc691871dc85a9ebe28e6ea3234ae631e
Deng William <DengWilliam45@gmail.com>
2026-02-13 08:32:52 +0000

    Some part at the top is not seen in the form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <body className="font-body antialiased flex flex-col min-h-screen bg-background">
         <FirebaseProvider>
           <SiteHeader />
-          <main className="flex-1">
+          <main className="flex-1 pt-16">
             {children}
           </main>
           <SiteFooter />
